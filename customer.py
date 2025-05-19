e54e15a07cdfd3f3d91db85fdb7a9830a7d5cf03
Cristina <kamuthucristina@gmail.com>
2025-05-19 11:58:03 +0300

    customer

---
+++ customer.py
@@ -0,0 +1,19 @@
+class Customer:
+    def __init__(self, name):
+        self.name = name 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str):
+            if 1 <= len(name) <= 15:
+                self._name = name
+            else:
+                raise ValueError("name should be between 1 - 15 characters")
+        else:
+            raise ValueError("name should be a string")
+
+c1 = Customer("Tie")
+print(c1.name)
